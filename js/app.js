c712cb1e0e706e7729b491dacef45ac9903e95e7
Omar-Ashinawy <omarashinawy@gmail.com>
2020-07-17 22:30:26 +0200

    Initial Look

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let menuBtn = document.getElementById("menuBtn");
+let closeBtn = document.querySelectorAll(".closeBtn");
+let menu = document.querySelector(".menu-container");
+// let content = document.querySelector(".content");
+// let body = document.body;
+
+menuBtn.addEventListener("change", toggleMenu);
+function toggleMenu() {
+    if (menuBtn.checked == true){
+        menu.style.right = "0";
+    }
+}
+
+closeBtn.forEach(function (element) {
+    element.addEventListener("click", function () {
+        menu.style.right = "-317px";
+        menuBtn.checked = false;
+        console.log("clicked");
+    });
+});
